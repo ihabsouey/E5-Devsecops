379d5af71196a07101162055db4cd25ea77c3523
ihabsouey <ihab.souey@gmail.com>
2025-07-11 12:27:03 +0200

    Implement code changes to enhance functionality and improve performance

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,7 @@
-import React from 'react';
 import { Box, Typography, Link } from '@mui/material';
 import useStyles from '../styles/styles';
 
 const Footer = () => {
-  const date = new Date().getFullYear();
   const classes = useStyles();
 
   return (
